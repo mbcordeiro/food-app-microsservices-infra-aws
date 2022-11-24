f50f0ff669fe5d82f954728950f5bf7f812cebd9
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-24 19:04:04 -0300

    Config vpc

--- src/main/java/com/matheuscordeiro/FoodAppAwsInfraStack.java
+++ src/main/java/com/matheuscordeiro/FoodAppAwsInfraStack.java
@@ -3,15 +3,13 @@ package com.matheuscordeiro;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class FoodAppAwsInfraStack extends Stack {
-    public AluraAwsInfraStack(final Construct scope, final String id) {
+    public FoodAppAwsInfraStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
 
-    public AluraAwsInfraStack(final Construct scope, final String id, final StackProps props) {
+    public FoodAppAwsInfraStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
         // The code that defines your stack goes here
---
+++ src/main/java/com/matheuscordeiro/FoodAppVpcStack.java
@@ -0,0 +1,24 @@
+package com.matheuscordeiro;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.constructs.Construct;
+
+public class FoodAppVpcStack extends Stack {
+    private Vpc vpc;
+
+    public FoodAppVpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public FoodAppVpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        vpc = Vpc.Builder.create(this, "FoodAppVpc")
+                .maxAzs(3)
+                .build();
+    }
+}
